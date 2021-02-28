bfe4e41fddf7fbd2df3ca4c4fba2bd13875d286c
Rafael Moreno <Rafael Moreno@LAPTOP-ROGBR6G0>
2021-02-27 20:43:51 -0600

    tarea1

---
+++ src/tipodedatosarea60066/TipoDeDatosArea60066.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatosarea60066;
+
+/**
+ *
+ * @author Rafael Moreno
+ */
+public class TipoDeDatosArea60066 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int lado,ancho;
+        lado = 7;
+        ancho = 4;
+        int area;
+        area = (ancho * lado);
+        System.out.println("El area es de: " + area);
+    }
+    
+}
